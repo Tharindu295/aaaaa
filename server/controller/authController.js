b3f271fa1a6add419b7038bbe8f6d59c767b21a0
It22638304@ <tbandara295@gmail.com>
2025-03-01 12:37:33 +0530

    comm

---
+++ server/controller/authController.js
@@ -0,0 +1,29 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import userModel from '../models/userModels.js';
+
+export const register = async (req,res)=>{
+
+    const {name,email,password} = req.body;
+
+    if(!name || !email || !password){
+        return res.status(400).json({
+            success:false,
+            message:"Missing Details"
+        })
+        
+
+    }
+
+    try {
+
+        const exsitingUser = await userModel.findOne({email}) 
+
+        const hashedpassword = await bcrypt.hash(password,10);
+        
+    } catch (error) {
+
+        res.json({success:false, message:error.message})
+        
+    }
+}
---
+++ server/models/userModels.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    verifyotp: {
+        type: String,
+        default: ""
+    },
+    verifyotpexpireAt: {
+        type: Number,
+        default: 0
+    },
+    isAccountVerified: {
+        type: Boolean,
+        default: false
+    },
+    resetOtp: {
+        type: String,
+        default: ""
+    },
+    resetOtpExpireAt: {
+        type: Number,
+        default: 0
+    },
+
+
+})
+
+const userModel = mongoose.models.user || mongoose.model('User', userSchema);
+
+
+export default userModel;
